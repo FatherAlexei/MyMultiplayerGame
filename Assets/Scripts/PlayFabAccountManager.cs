3c3699eba5cfe6fc3e08288f0c6de70f24032a1e
FatherAlexei <68125662+FatherAlexei@users.noreply.github.com>
2024-01-11 22:28:18 -0800

    Homework

--- Assets/Scripts/PlayFabAccountManager.cs
+++ Assets/Scripts/PlayFabAccountManager.cs
@@ -2,20 +2,38 @@ using PlayFab;
 using TMPro;
 using UnityEngine;
 using PlayFab.ClientModels;
+using System;
+using System.Collections.Generic;
 
 public class PlayFabAccountManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text titleLabel;
+    [SerializeField] private TMP_Text playerInfo;
+    [SerializeField] private TMP_Text itemsInfo;
 
     private void Start()
     {
         PlayFabClientAPI.GetAccountInfo(new GetAccountInfoRequest(), OnGetAccount, OnError);
+        PlayFabClientAPI.GetCatalogItems(new GetCatalogItemsRequest(), OnGetCatalog, OnError);
     }
 
 
+    private void OnGetCatalog(GetCatalogItemsResult result)
+    {
+        Debug.Log("OnGetCatalogSuccess");
+        ShowItems(result.Catalog);
+    }
+
+    private void ShowItems(List<CatalogItem> catalog)
+    {
+        foreach (var item in catalog)
+        {
+            itemsInfo.text += "\n" + item.ItemId;
+        }
+    }
+
     private void OnGetAccount(GetAccountInfoResult result)
     {
-        titleLabel.text = $"Playfab id: {result.AccountInfo.PlayFabId} \n" +
+        playerInfo.text = $"Playfab id: {result.AccountInfo.PlayFabId} \n" +
             $"Username: {result.AccountInfo.Username}\n" +
             $"GameCenter info: {result.AccountInfo.GameCenterInfo}";
     }
